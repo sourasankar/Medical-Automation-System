1ce04a2200c06c866b750ed27148cafaad72896c
sourakootty <soura.kootty4@gmail.com>
2021-11-29 12:33:57 +0530

    Update generateCashReceipt.php

--- generateCashReceipt.php
+++ generateCashReceipt.php
@@ -30,6 +30,102 @@
 
           <div class="card">
             <div class="card-body" >
+              
+              <form method="POST" action="#">
+                <h5 class="card-title">Invoice</h5>
+                <div class="">
+                    <table class="table table-striped table-bordered">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Batch No</th>
+                          <th scope="col">Medicine Id</th>
+                          <th scope="col">Medicine Name</th>
+                          <th scope="col">Vendor</th>
+                          <th scope="col">Exp.</th>
+                          <th scope="col">Qty.</th>
+                          <th scope="col">Price</th>
+                          <th scope="col"></th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr class="align-middle">
+                          <th scope="row">1</th>
+                          <td>9874662</td>
+                          <td>8456</td>
+                          <td>Paracetamol 650</td>
+                          <td>Alkem</td>
+                          <td>Dec-2022</td>
+                          <td><input onchange="calPrice(this.value,1)" type="number" class="form-control-sm" style="width: 70px;" name="med1" value="1" min=1></td>
+                          <td id="med1">10/-</td>
+                          <td>
+                              <form method="POST" action="#">
+                                  <button type="submit" class="btn btn-danger btn-sm" name="delete" value="dsad465ad5"><i class="bi bi-trash"></i></button>
+                              </form>
+                          </td>                         
+                        </tr>
+                        <tr class="align-middle">
+                          <th scope="row">1</th>
+                          <td>9874662</td>
+                          <td>8456</td>
+                          <td>Paracetamol 650</td>
+                          <td>Alkem</td>
+                          <td>Dec-2022</td>
+                          <td><input onchange="calPrice(this.value,2)" type="number" class="form-control-sm" style="width: 70px;" name="med2" value="1" min=1></td>
+                          <td id="med2">20/-</td>
+                          <td>
+                              <form method="POST" action="#">
+                                  <button type="submit" class="btn btn-danger btn-sm" name="delete" value="dsad465ad5"><i class="bi bi-trash"></i></button>
+                              </form>
+                          </td>                         
+                        </tr>
+                        <tr class="align-middle">
+                          <th scope="row">1</th>
+                          <td>9874662</td>
+                          <td>8456</td>
+                          <td>Paracetamol 650</td>
+                          <td>Alkem</td>
+                          <td>Dec-2022</td>
+                          <td><input onchange="calPrice(this.value,3)" type="number" class="form-control-sm" style="width: 70px;" name="med3" value="1" min=1></td>
+                          <td id="med3">30/-</td>
+                          <td>
+                              <form method="POST" action="#">
+                                  <button type="submit" class="btn btn-danger btn-sm" name="delete" value="dsad465ad5"><i class="bi bi-trash"></i></button>
+                              </form>
+                          </td>                         
+                        </tr>                       
+                        <tr class="align-middle">
+                          <th scope="row">1</th>
+                          <td>9874662</td>
+                          <td>8456</td>
+                          <td>Paracetamol 650</td>
+                          <td>Alkem</td>
+                          <td>Dec-2022</td>
+                          <td><input onchange="calPrice(this.value,4)" type="number" class="form-control-sm" style="width: 70px;" name="med4" value="1" min=1></td>
+                          <td id="med4">40/-</td>
+                          <td>
+                              <form method="POST" action="#">
+                                  <button type="submit" class="btn btn-danger btn-sm" name="delete" value="dsad465ad5"><i class="bi bi-trash"></i></button>
+                              </form>
+                          </td>                         
+                        </tr>
+                      </tbody>
+                    </table>
+                    <div class="align-middle" style="font-weight: 700;display: flex;justify-content: center;align-items: center;">
+                      
+                      <div id="total">Total (Rs): 100/-</div>
+                      <button type="submit" class="btn btn-primary btn-sm" style="margin-left: 25px;" name="generate">Generate</button>
+                      
+                    </div>
+                </div>
+                </form>
+
+            </div>
+
+          </div>
+
+          <div class="card">
+            <div class="card-body" >
 
                 <div class="row g-3" style="margin-top: 20px;">
                     <div class="col-md-4 col-lg-3 col-xxl-2">
@@ -74,7 +170,25 @@
                           <td>R5</td> 
                           <td>
                               <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
+                              </form>
+                          </td>                         
+                        </tr>   
+                        <tr class="align-middle">
+                          <th scope="row">1</th>
+                          <td>9874662</td>
+                          <td>8456</td>
+                          <td>Paracetamol 650</td>
+                          <td>Painkiller</td>
+                          <td>Alkem</td>
+                          <td>Nov-2021</td>
+                          <td>Dec-2022</td>
+                          <td>10</td>
+                          <td>120/-</td>
+                          <td>R5</td> 
+                          <td>
+                              <form method="POST" action="#">
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
                               </form>
                           </td>                         
                         </tr>
@@ -92,7 +206,7 @@
                           <td>R5</td> 
                           <td>
                               <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
                               </form>
                           </td>                         
                         </tr>
@@ -110,7 +224,7 @@
                           <td>R5</td> 
                           <td>
                               <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
                               </form>
                           </td>                         
                         </tr>
@@ -128,7 +242,7 @@
                           <td>R5</td> 
                           <td>
                               <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
                               </form>
                           </td>                         
                         </tr>
@@ -146,7 +260,7 @@
                           <td>R5</td> 
                           <td>
                               <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
                               </form>
                           </td>                         
                         </tr>
@@ -164,46 +278,10 @@
                           <td>R5</td> 
                           <td>
                               <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
+                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</button>
                               </form>
                           </td>                         
-                        </tr>
-                        <tr class="align-middle">
-                          <th scope="row">1</th>
-                          <td>9874662</td>
-                          <td>8456</td>
-                          <td>Paracetamol 650</td>
-                          <td>Painkiller</td>
-                          <td>Alkem</td>
-                          <td>Nov-2021</td>
-                          <td>Dec-2022</td>
-                          <td>10</td>
-                          <td>120/-</td>
-                          <td>R5</td> 
-                          <td>
-                              <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
-                              </form>
-                          </td>                         
-                        </tr>
-                        <tr class="align-middle">
-                          <th scope="row">1</th>
-                          <td>9874662</td>
-                          <td>8456</td>
-                          <td>Paracetamol 650</td>
-                          <td>Painkiller</td>
-                          <td>Alkem</td>
-                          <td>Nov-2021</td>
-                          <td>Dec-2022</td>
-                          <td>10</td>
-                          <td>120/-</td>
-                          <td>R5</td> 
-                          <td>
-                              <form method="POST" action="#">
-                                  <button type="submit" class="btn btn-primary btn-sm" name="add" value="dsad465ad5">ADD</i></button>
-                              </form>
-                          </td>                         
-                        </tr>
+                        </tr>        
 
                       </tbody>
                     </table>
@@ -216,6 +294,24 @@
         </div>        
     
   </main><!-- End #main -->
+
+  <script>
+    var price=[10,20,30,40];
+    var itemPrice=[10,20,30,40];
+
+    function calPrice(qty,n){
+      var product = qty*price[n-1];
+      itemPrice[n-1] = product;
+      var i=0,sum=0;
+      while(itemPrice[i]!=null){
+        sum+=itemPrice[i];
+        i++;
+      }
+      document.getElementById("med"+n).innerHTML=product+"/-";
+      document.getElementById("total").innerHTML="Total (Rs): "+sum+"/-";
+    }
+
+  </script>
   
   <?php require "assets/php/footer.php"; ?>
 

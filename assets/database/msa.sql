62ad8b1cd047638cc4899b050896afe1bbcd8678
sourakootty <soura.kootty4@gmail.com>
2021-12-02 12:43:50 +0530

    final signing off

--- assets/database/msa.sql
+++ assets/database/msa.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Dec 01, 2021 at 07:47 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.12
+-- Host: 127.0.0.1
+-- Generation Time: Dec 02, 2021 at 08:06 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -63,11 +64,12 @@ CREATE TABLE `medicine` (
 --
 
 INSERT INTO `medicine` (`medicine_id`, `name`, `category`, `vendor_id`, `rack`, `required`) VALUES
-(1, 'test1', 'cat1', 2, 'R5', '0'),
+(1, 'test1', 'cat1', 2, 'R5', '3'),
 (2, 'test2', 'cat2', 3, 'T4', '0'),
 (3, 'test3', 'cat3', 4, 't5', '0'),
 (4, 'test4', 'cat4', 2, 'fds', '0'),
-(5, 'gfdg', 'gfdgfd', 3, 'gf', '0');
+(5, 'gfdg', 'gfdgfd', 3, 'gf', '0'),
+(6, 'Paracetamol', 'painkiller', 6, 'R7', '0');
 
 -- --------------------------------------------------------
 
@@ -91,7 +93,7 @@ CREATE TABLE `medicine_stock` (
 --
 
 INSERT INTO `medicine_stock` (`medicine_stock_id`, `medicine_id`, `batch_no`, `mfg`, `exp`, `quantity`, `purchase_price`, `selling_price`) VALUES
-(5, 1, 'test1B', '2021-12-01', '2023-06-30', '80', '120', '200'),
+(5, 1, 'test1B', '2021-12-01', '2023-06-30', '77', '120', '200'),
 (6, 3, 'test3B', '2021-12-01', '2023-02-28', '35', '150', '250'),
 (14, 3, 'kjhkh', '2021-07-01', '2021-10-31', '4', '1', '1'),
 (15, 1, 'jkh', '2021-06-01', '2021-07-31', '1', '765', '765'),
@@ -100,9 +102,8 @@ INSERT INTO `medicine_stock` (`medicine_stock_id`, `medicine_id`, `batch_no`, `m
 (22, 4, 'ghf', '2021-07-01', '2021-07-31', '2', '6546', '564'),
 (30, 4, 'tre', '2021-10-01', '2021-07-31', '4', '45', '5454'),
 (31, 3, '543', '2021-06-01', '2021-07-31', '4', '54', '545'),
-(33, 1, 'kootty', '2021-12-01', '2022-11-30', '5', '100', '200'),
-(34, 2, 'soura', '2021-12-01', '2021-12-31', '1', '10', '50'),
-(35, 1, 'something', '2021-07-01', '2022-01-31', '1', '10', '100');
+(36, 2, 'D240', '2021-10-01', '2023-06-30', '0', '100', '150'),
+(37, 6, 'DT2562', '2021-11-01', '2023-06-30', '15', '100', '150');
 
 -- --------------------------------------------------------
 
@@ -132,7 +133,9 @@ INSERT INTO `purchases` (`purchase_id`, `medicine_id`, `date`, `purchase_price`,
 (6, 5, '2021-12-01', '54', '3', 'YES'),
 (7, 1, '2021-12-01', '100', '5', 'YES'),
 (8, 2, '2021-12-01', '10', '1', 'YES'),
-(9, 1, '2021-12-01', '10', '1', 'YES');
+(9, 1, '2021-12-01', '10', '1', 'YES'),
+(10, 2, '2021-12-02', '100', '15', 'YES'),
+(11, 6, '2021-12-02', '100', '15', 'YES');
 
 -- --------------------------------------------------------
 
@@ -165,7 +168,9 @@ INSERT INTO `sales` (`invoice_id`, `date_time`, `total_amount`) VALUES
 ('20211201064340', '2021-12-01 06:43:40', '1500'),
 ('20211201064359', '2021-12-01 06:43:59', '1750'),
 ('20211201065138', '2021-12-01 06:51:38', '1150'),
-('20211201065207', '2021-12-01 06:52:07', '1100');
+('20211201065207', '2021-12-01 06:52:07', '1100'),
+('20211202063931', '2021-12-02 06:39:31', '2250'),
+('20211202080152', '2021-12-02 08:01:52', '600');
 
 -- --------------------------------------------------------
 
@@ -186,43 +191,13 @@ CREATE TABLE `sold_medicine` (
 --
 
 INSERT INTO `sold_medicine` (`sold_item_id`, `invoice_id`, `medicine_id`, `quantity`, `price`) VALUES
-(5, '20211201045645', 1, '3', '200'),
-(6, '20211201045645', 3, '2', '250'),
-(7, '20211201051246', 1, '2', '200'),
-(8, '20211201051246', 3, '3', '250'),
-(9, '20211201051338', 1, '2', '200'),
-(10, '20211201051338', 3, '3', '250'),
-(11, '20211201051533', 1, '2', '200'),
-(12, '20211201051533', 3, '3', '250'),
-(13, '20211201061605', 1, '2', '200'),
-(14, '20211201061611', 1, '1', '200'),
-(15, '20211201061623', 1, '1', '200'),
-(16, '20211201061705', 1, '1', '200'),
-(17, '20211201061824', 1, '2', '200'),
-(18, '20211201061953', 1, '2', '200'),
-(19, '20211201061953', 3, '3', '250'),
-(20, '20211201062122', 1, '2', '200'),
-(21, '20211201062152', 1, '1', '200'),
-(22, '20211201062152', 3, '1', '250'),
-(23, '20211201062255', 1, '1', '200'),
-(24, '20211201062316', 1, '1', '200'),
-(25, '20211201062316', 3, '1', '250'),
-(26, '20211201062551', 1, '2', '200'),
-(27, '20211201062551', 3, '3', '250'),
-(28, '20211201063947', 1, '1', '200'),
-(29, '20211201063947', 3, '2', '250'),
-(30, '20211201064022', 1, '1', '200'),
-(31, '20211201064022', 3, '1', '250'),
 (32, '20211201064234', 1, '5', '200'),
-(33, '20211201064234', 3, '5', '250'),
 (34, '20211201064340', 1, '5', '200'),
-(35, '20211201064340', 3, '2', '250'),
 (36, '20211201064359', 1, '5', '200'),
-(37, '20211201064359', 3, '3', '250'),
 (38, '20211201065138', 1, '2', '200'),
-(39, '20211201065138', 3, '3', '250'),
 (40, '20211201065207', 1, '3', '200'),
-(41, '20211201065207', 3, '2', '250');
+(42, '20211202063931', 2, '15', '150'),
+(43, '20211202080152', 1, '3', '200');
 
 -- --------------------------------------------------------
 
@@ -246,8 +221,10 @@ CREATE TABLE `vendor` (
 
 INSERT INTO `vendor` (`vendor_id`, `name`, `address`, `phone`, `email`, `account_no`, `ifsc_code`) VALUES
 (2, 'Alkem', 'Mumbai, India', '9876543210', 'something@alkem.com', '541456164164', 'SBIN4564IN'),
-(3, 'hgf', 'hgfh', 'hgf', 'hgf@gfd', '765', '564hf'),
-(4, 'fdsfds', 'fdsf', 'dsf', 'dsfds@gfd', '543', '543543');
+(3, 'hgf', 'hgfh', '6451324316', 'hgf@gfd', '765', 'SBIN5615'),
+(4, 'fdsfds', 'fdsf', '6491364875', 'dsfds@gfd', '543', 'SBIN65'),
+(5, 'Natcom', 'acb, india', '9876543210', 'some@xyz.com', '645137942', 'SBIN4564'),
+(6, 'Cipla', 'mumbai,india', '9765412364', 'some@abc.com', '8975465155', 'SBIN45684');
 
 --
 -- Indexes for dumped tables
@@ -303,31 +280,31 @@ ALTER TABLE `vendor`
 -- AUTO_INCREMENT for table `medicine`
 --
 ALTER TABLE `medicine`
-  MODIFY `medicine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `medicine_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `medicine_stock`
 --
 ALTER TABLE `medicine_stock`
-  MODIFY `medicine_stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `medicine_stock_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT for table `purchases`
 --
 ALTER TABLE `purchases`
-  MODIFY `purchase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `purchase_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `sold_medicine`
 --
 ALTER TABLE `sold_medicine`
-  MODIFY `sold_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `sold_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `vendor`
 --
 ALTER TABLE `vendor`
-  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `vendor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
